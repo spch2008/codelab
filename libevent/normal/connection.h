e5c98d9edaa5b295a32ee893955dccf6252d2e97
spch2008 <spch2008@foxmail.com>
2016-07-05 09:18:38 +0800

    ADD: add connection structure

---
+++ libevent/normal/connection.h
@@ -0,0 +1,28 @@
+/**
+* @file   connection.h
+* @author sunpengcheng(spch2008@foxmail.com)
+* @date   2016-07-05 09:12:07
+* @brief
+**/
+
+#ifndef _CONNECTION_H_
+#define _CONNECTION_H_
+
+#include <event.h>
+
+struct Connection
+{
+    int fd;
+    int left_size;
+
+    char *read_buf;
+    int   read_size;
+
+    char *write_buf;
+    int   write_size;
+
+    struct event *revent;
+    struct event *wevent;
+};
+
+#endif
