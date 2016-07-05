a14ed4110b45841734b4eb6c951d7c5ec79bbd16
spch2008 <spch2008@foxmail.com>
2016-07-05 09:11:07 +0800

    ADD: add data header

---
+++ libevent/normal/data_header.h
@@ -0,0 +1,17 @@
+/**
+* @file   data_header.h
+* @author sunpengcheng(spch2008@foxmail.com)
+* @date   2016-07-05 09:08:45
+* @brief
+**/
+
+#ifndef _DATA_HEADER_H_
+#define _DATA_HEADER_H_
+
+struct DataHeader
+{
+    int magic_num;
+    int data_len;
+};
+
+#endif
